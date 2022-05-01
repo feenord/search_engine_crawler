485a210b56b404c42e7a57703cdf9d44461e5876
feenord <feenord40@gmail.com>
2022-05-01 20:03:09 +0300

    search_engine

--- scripts/install_mongodb.sh
+++ scripts/install_mongodb.sh
@@ -4,6 +4,8 @@
 #echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update -y
 sudo apt install mongodb-server -y
+git clone -b master https://github.com/feenord/search_engine_crawler.git
+cd ./seacrh_engine_crawler/
 cp ./scripts/mongodb.conf /etc/
 sudo systemctl start mongodb
 sudo systemctl enable mongodb
