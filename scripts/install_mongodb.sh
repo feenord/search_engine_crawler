05421877b6f5f1f874756552122e70154546be72
feenord <feenord40@gmail.com>
2022-05-01 18:35:18 +0300

    add config mongo db

--- scripts/install_mongodb.sh
+++ scripts/install_mongodb.sh
@@ -4,6 +4,7 @@
 #echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update -y
 sudo apt install mongodb-server -y
+cp ./scripts/mongodb.conf /etc/
 sudo systemctl start mongodb
 sudo systemctl enable mongodb
 sudo systemctl status mongodb
